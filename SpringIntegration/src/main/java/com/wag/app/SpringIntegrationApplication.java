40cbd9433bae7caeb1fe714d97b3b8fa15e31d37
Ravi Gupta <43459486+RaviGuptaAutomation@users.noreply.github.com>
2021-10-19 14:25:17 +0530

    done

--- SpringIntegration/src/main/java/com/wag/app/SpringIntegrationApplication.java
+++ SpringIntegration/src/main/java/com/wag/app/SpringIntegrationApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringIntegrationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringIntegrationApplication.class, args); 
+		SpringApplication.run(SpringIntegrationApplication.class, args);
+		System.out.println("Hello");
 	}
 
 }
